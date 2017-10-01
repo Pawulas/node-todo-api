4a783d1c475ec0f0ba947d535a5457ef6b51f2e2
JCOMMERCE\patk <pawel.tkocz@jcommerce.pl>
2017-10-01 21:27:22 +0200

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,11 +7,13 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
 
 
 app.post('/todos', (req, res) =>{
@@ -112,7 +114,9 @@ app.post('/users', (req, res) => {
   });
 });
 
-
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user); 
+});
 
 
 app.listen(port, () => {
