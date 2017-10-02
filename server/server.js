c9384d60102c05c14f656fddec86f41ad9aec1a0
JCOMMERCE\patk <pawel.tkocz@jcommerce.pl>
2017-10-02 21:20:35 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 
-
 app.post('/todos', (req, res) =>{
   var todo = new Todo({
     text: req.body.text
@@ -116,6 +115,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user); 
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header("x-auth", token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
 });
 
 
